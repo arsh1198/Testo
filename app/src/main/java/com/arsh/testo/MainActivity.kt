310be5d21364306c36a737de1ed85ea6da942c83
turbo <turbonater62@gmail.com>
2020-06-26 19:24:54 +0530

    SignUp Layout

---
+++ app/src/main/java/com/arsh/testo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.arsh.testo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    lateinit var btnSignUp: Button
+    lateinit var txtEmail: TextInputLayout
+    lateinit var txtUserName: TextInputLayout
+    lateinit var txtPassword: TextInputLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        btnSignUp = findViewById(R.id.btnSignUp)
+        txtEmail = findViewById(R.id.txtSignUpEmail)
+        txtUserName = findViewById(R.id.txtSignUpUserId)
+        txtPassword = findViewById(R.id.txtPassword)
+
+        val email = txtEmail.editText?.text
+        val password = txtPassword.editText?.text
+
+        btnSignUp.setOnClickListener {
+            signUp(email.toString(), password.toString())
+        }
+
+
+    }
+
+    fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Welcome! ".plus(auth.currentUser?.email),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
